59d224f86675a879ecdff6fb7844fe30ae474cc6
Floyd08 <119715701+Floyd08@users.noreply.github.com>
2023-03-15 18:37:38 -0400

    Add files via upload

--- CocktailGenerator/src/main/webapp/angular-front-end/src/app/drink-view/drink-view.component.ts
+++ CocktailGenerator/src/main/webapp/angular-front-end/src/app/drink-view/drink-view.component.ts
@@ -2,10 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { RecipeService } from '../services/recipe-service.service';
 
-interface Template {
-	value: number;
-	viewValue: string;
-}
+//interface Template {
+	//value: number;
+	//viewValue: string;
+//}
 
 @Component({
   selector: 'app-drink-view',
@@ -15,16 +15,29 @@ interface Template {
 export class DrinkViewComponent implements OnInit {
 	
 	templateNames: string[] = [];			//populate this array from the backend
+	templateSelect: string = "";
 	
 	constructor(private RS: RecipeService) {}
 	
 	ngOnInit() {
 		this.RS.getNames().subscribe(data => {
 			this.templateNames = data;
-			console.log(this.templateNames);
+			//console.log(this.templateNames);
+		})
+		this.RS.getRecipe(0).subscribe(data => {
+			this.templateSelect = data;
+			//console.log(this.templateSelect);
+		})
+	}
+	
+	getRecipe(index: number) {
+		this.RS.getRecipe(index).subscribe(data => {
+			this.templateSelect = data;
+			//console.log(this.templateSelect);
+			document.getElementById("recipeView")!.innerHTML = this.templateSelect;
 		})
 		
-		//this.templateNames = this.RS.getNames;
+		
 	}
 
 }
